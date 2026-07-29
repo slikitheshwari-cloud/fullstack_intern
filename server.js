f62573a39db9b2af9d26245f3c519186f9e342bf
S.Likitheshwari <slikitheshwari@gmail.com>
2026-07-29 19:03:49 +0530

    Day 5 Express GET and POST routes

--- server.js
+++ server.js
@@ -4,10 +4,32 @@ const app = express();
 
 const PORT = 3000;
 
+// Middleware to read JSON data
+app.use(express.json());
+
+// GET Route
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to My Express Server!");
 });
 
+// GET Route
+app.get("/about", (req, res) => {
+    res.send("This is the About Page.");
+});
+
+// POST Route
+app.post("/blog", (req, res) => {
+
+    const blog = req.body;
+
+    res.json({
+        message: "Blog received successfully!",
+        data: blog
+    });
+
+});
+
+// Start Server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
